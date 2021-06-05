94acc3b5bd50e7e7277798bf2e8eedd72e00cec0
Robin Hui <robin@Robin-MacBook-2020.local>
2021-06-05 17:16:35 +0800

    init

--- src/middlewares/log.ts
+++
@@ -1,8 +0,0 @@
-import * as Koa from 'koa';
-
-export default async function(ctx: Koa.Context, next: () => Promise<any>): Promise<any> {
-  const start: number = Date.now();
-  await next();
-  const ms: number = Date.now() - start;
-  global.logger.info(`${ctx.method} ${ctx.url} ${ctx.status} - ${ms}ms`);
-}
